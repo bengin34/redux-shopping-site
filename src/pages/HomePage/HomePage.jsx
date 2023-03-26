ba4289a0676f53f7d8873c097b85622657be4a9e
bengin34 <becaglar3434@gmail.com>
2023-03-26 23:39:38 +0300

    home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Slider from "../../components/Slider/Slider"
 import "./HomePage.scss"
+import {Category} from ".."
+import ProductList from "../../components/ProductList/ProductList"
+import { useSelector, useDispatch } from 'react-redux'
+import {fetchCategories, fetchProductsByCategory} from "../../store/categorySlice"
 
 
 const HomePage = () => {
+const dispatch = useDispatch()
+const {data: categories, status: categoryStatus} = useSelector((state) => state.category)
   return (
-    <div>
+    <div className='home-page' >
       <Slider/>
+      <Category categories= { categories} status={categoryStatus} />
     </div>
   )
 }
